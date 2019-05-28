87533329c998debec7ae92b4b8481e21c15efd10
kbbushman <k.bushman@yahoo.com>
2019-05-27 20:10:39 -0700

    Adds bcrypt and register user schema and resolver

--- schema/user.js
+++ schema/user.js
@@ -14,6 +14,6 @@ export default `
   }
 
   type Mutation {
-    createUser(username: String!, email: String!, password: String!): User!
+    register(username: String!, email: String!, password: String!): Boolean!
   }
 `;
