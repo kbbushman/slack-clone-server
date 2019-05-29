b8fa4a8f361ecf91b498237a66529c507ace1013
kbbushman <k.bushman@yahoo.com>
2019-05-28 20:03:19 -0700

    Adds error schema and validation to user model

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,12 @@
 import bcrypt from 'bcrypt';
+import _ from 'lodash';
+
+const formatErrors = (err, models) => {
+  if (err instanceof models.Sequelize.ValidationError) {
+    return err.errors.map(error => _.pick(error, ['path', 'message']));
+  }
+  return [{ path: 'name', message: 'Something went wrong. Please try again' }];
+}
 
 export default {
   Query: {
@@ -8,11 +16,29 @@ export default {
   Mutation: {
     register: async (parent, { password, ...otherArgs }, { models }) => {
       try {
+        if (password.length < 5 || password.length > 100) {
+          return {
+            ok: false,
+            errors: [
+              {
+                path: 'password',
+                message: 'The password must be between 5 and 100 characters',
+              },
+            ],
+          };
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
-        await models.User.create({ ...otherArgs, password: hashedPassword });
-        return true;
+        const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+        return {
+          ok: true,
+          user,
+        };
       } catch (err) {
-        return false;
+        return {
+          ok: false,
+          errors: formatErrors(err, models),
+        };
       }
     },
   },
