3aa863455d82256da09f6e318aa2bf4eec44d341
kbbushman <k.bushman@yahoo.com>
2019-05-30 10:27:26 -0700

    Adds custom setCurrentUser middleware

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 import path from 'path';
 import express from 'express';
+import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
-import cors from 'cors';
 import models from './models';
 import keys from './config/keys';
+import setCurrentUser from './middleware/setCurrentUser';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
-
-const app = express();
 const endPoint = '/graphql';
-const PORT = process.env.PORT || 5000;
+
+// CORS Middleware
 app.use(cors('*'));
+
+// Auth Middleware
+app.use((req, res, next) => setCurrentUser(req, res, next));
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
+  context: ({ req }) => ({
     models,
-    user: {
-      id: 1,
-    },
+    user: req.currentUser,
     SECRET: keys.SECRET,
     SECRET2: keys.SECRET2,
-  },
+  }),
 });
 server.applyMiddleware({ app, endPoint });
 
