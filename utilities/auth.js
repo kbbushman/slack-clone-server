3aed6113d8bf6818466b09d393c2f6f24c51dec4
kbbushman <k.bushman@yahoo.com>
2019-06-01 23:33:59 -0700

    Adds username to token

--- utilities/auth.js
+++ utilities/auth.js
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt';
 
 const createTokens = async (user, secret, secret2) => {
   const createToken = jwt.sign(
-    { user: _.pick(user, ['id']) },
+    { user: _.pick(user, ['id', 'username']) },
     secret,
     { expiresIn: '1h' },
   );
