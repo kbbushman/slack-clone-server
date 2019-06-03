24b057e4b21a690183171972264cf43c8c65e77e
kbbushman <k.bushman@yahoo.com>
2019-06-03 08:54:54 -0700

    Refactors createChannel response from boolean to channel object

--- schema/channel.js
+++ schema/channel.js
@@ -9,7 +9,13 @@ export default gql`
     users: [User!]!
   }
 
+  type ChannelResponse {
+    ok: Boolean!
+    channel: Channel
+    errors: [Error!]
+  }
+
   type Mutation {
-    createChannel(teamId: Int!, name: String!, public: Boolean=false): Boolean!
+    createChannel(teamId: Int!, name: String!, public: Boolean=false): ChannelResponse!
   }
 `;
