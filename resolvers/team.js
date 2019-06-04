bbf70dab64838273c45681b32642aa691a486db1
kbbushman <k.bushman@yahoo.com>
2019-06-03 18:14:23 -0700

    Adds addTeamMember mutation

--- resolvers/team.js
+++ resolvers/team.js
@@ -7,6 +7,35 @@ export default {
       models.Team.findAll({ where: { owner: user.id }}, { raw: true })
   )},
   Mutation: {
+    addTeamMember: authRequired.createResolver(async (parent, { email, teamId }, { models, user }) => {
+      try {
+        const teamPromise = models.Team.findOne({ where: { id: teamId }}, { raw: true });
+        const userToAddPromise = models.User.findOne({ where: { email }}, { raw: true });
+        const [team, userToAdd] = await Promise.all([teamPromise, userToAddPromise]);
+        if (team.owner !== user.id) {
+          return {
+            ok: false,
+            errors: [{ path: 'email', message: 'You do not have permission to add members to this team' }],
+          };
+        }
+        if (!userToAdd) {
+          return {
+            ok: false,
+            errors: [{ path: 'email', message: `Could not find a user with email address ${email}` }],
+          };
+        }
+        await models.Member.create({ userId: userToAdd.id, teamId });
+        return {
+          ok: true,
+        };
+      } catch (err) {
+        console.log(err);
+        return {
+          ok: false,
+          errors: formatErrors(err),
+        };
+      }
+    }),
     createTeam: authRequired.createResolver(async (parent, args, { models, user }) => {
       try {
         const team = await models.Team.create({ ...args, owner: user.id });
@@ -21,7 +50,7 @@ export default {
           ok: false,
           errors: formatErrors(err),
         };
-      };
+      }
     }),
   },
   Team: {
